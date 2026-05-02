e760a243f2eeba849ffacf0f050cf9a79bb94b52
Kauê Brito <kauebritodev@gmail.com>
2026-05-02 18:48:41 -0300

    feat: adição de suporte offline ao firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyByWcvyS0sRIvmvigEq-e09_RTRV7HbQWA",
@@ -9,7 +9,15 @@ const firebaseConfig = {
   messagingSenderId: "888294277149",
   appId: "1:888294277149:web:c5d8f5586959fa08af46f4",
   measurementId: "G-0E33D1QBE9"
-};
+}
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+
+enableIndexedDbPersistence(db).catch((err) => {
+  if (err.code === 'failed-precondition') {
+    console.warn('Persistência offline falhou: múltiplas abas abertas.')
+  } else if (err.code === 'unimplemented') {
+    console.warn('Este navegador não suporta persistência offline.')
+  }
+})
