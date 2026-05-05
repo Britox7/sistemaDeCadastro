4dad43a98a6838fafde290e8b51cce68596fb417
Kauê Brito <kauebritodev@gmail.com>
2026-05-04 23:32:12 -0300

    feat: migração de dados e mudança de conta no firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByWcvyS0sRIvmvigEq-e09_RTRV7HbQWA",
-  authDomain: "cadastro-de-alunos---uniateneu.firebaseapp.com",
-  projectId: "cadastro-de-alunos---uniateneu",
-  storageBucket: "cadastro-de-alunos---uniateneu.firebasestorage.app",
-  messagingSenderId: "888294277149",
-  appId: "1:888294277149:web:c5d8f5586959fa08af46f4",
-  measurementId: "G-0E33D1QBE9"
-}
+  apiKey: "AIzaSyBxsMiNwFaT3hPHs_cfGVMEu-VD_aZcZTQ",
+  authDomain: "cadastrodealunosuniateneu.firebaseapp.com",
+  projectId: "cadastrodealunosuniateneu",
+  storageBucket: "cadastrodealunosuniateneu.firebasestorage.app",
+  messagingSenderId: "12209445701",
+  appId: "1:12209445701:web:280f4637c9fb13da71fc2c",
+  measurementId: "G-5DHY5H8RZN"
+};
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
