9220426b4a78f70a04d2dbf9d0393eab2f9d8207
Kauê Brito <kauebritodev@gmail.com>
2026-05-05 11:03:07 -0300

    feat: migrar credenciais firebase para variáveis de ambiente

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBxsMiNwFaT3hPHs_cfGVMEu-VD_aZcZTQ",
-  authDomain: "cadastrodealunosuniateneu.firebaseapp.com",
-  projectId: "cadastrodealunosuniateneu",
-  storageBucket: "cadastrodealunosuniateneu.firebasestorage.app",
-  messagingSenderId: "12209445701",
-  appId: "1:12209445701:web:280f4637c9fb13da71fc2c",
-  measurementId: "G-5DHY5H8RZN"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
